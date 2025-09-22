2f97c66847c71906f21e8877237306b1964b0468
kim2246 <139811409+kim2246@users.noreply.github.com>
2025-09-22 08:28:23 +0000

    Deploying to gh-pages from @ kim2246/kim2246.github.io@a52880e7d6c51ef8cd9fa51e5940123babe03331 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -112,11 +112,11 @@ ninja.data = [{
           description: "a project with no image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
+            },},{id: "projects-physiological-ux-of-conversational-interfaces",
+          title: 'Physiological UX of Conversational Interfaces',
+          description: "Evaluating interactive TV interfaces using EEG and HRV physiological measures",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
+              window.location.href = "/projects/conversational-ux/";
             },},{id: "projects-project-8",
           title: 'project 8',
           description: "an other project with a background image and giscus comments",
