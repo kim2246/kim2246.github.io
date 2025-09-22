a27c8b93b2d5233e5780f80e30658195f66bd932
kim2246 <139811409+kim2246@users.noreply.github.com>
2025-09-22 10:53:44 +0000

    Deploying to gh-pages from @ kim2246/kim2246.github.io@ac695c2e7e1634b281c6e4d152e48d51587c3bba 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -102,11 +102,6 @@ ninja.data = [{
           description: "another without an image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
             },},{id: "projects-companion-robot-interfaces-for-older-adults",
           title: 'Companion Robot Interfaces for Older Adults',
           description: "Experimental study on type, weight, and material preferences for elderly-friendly companion robots",
@@ -132,6 +127,11 @@ ninja.data = [{
           description: "Exploring driver emotional states, attention prediction, and driver–vehicle interaction frameworks for autonomous vehicles using deep learning, clustering, and think-aloud data",
           section: "Projects",handler: () => {
               window.location.href = "/projects/driver-attention-av/";
+            },},{id: "projects-persuasive-nudges-in-automated-driving",
+          title: 'Persuasive Nudges in Automated Driving',
+          description: "Exploring how social norm messages can persuade drivers to continue using driving automation instead of overriding",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/persuasive-nudges/";
             },},{
         id: 'social-email',
         title: 'email',
